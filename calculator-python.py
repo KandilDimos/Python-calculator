73231788958718a27578cdde658f9d6e9e0aef77
KandilDimos <142528384+KandilDimos@users.noreply.github.com>
2023-11-12 13:29:24 +0200

    Add files via upload

---
+++ calculator-python.py
@@ -0,0 +1,78 @@
+
+operator = input(f"Enter an operator (+ , - , * , / , ** ): ")
+num1 = float(input(f"Enter the 1st number: "))
+num2 = float(input(f"Enter the 2nd number: "))
+
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+elif operator == "**":
+    result = num1 ** num2
+    print(round(result, 3))
+
+
+else:
+    print(f"{operator} is not a valid operator")
+
+if num2 == 0:
+  print(f"You can't do a division by number 0. Try again.")
+
+
+if num1 == 0:
+  print(f"You can't do a division by number 0. Try again.")
+
+
+while True:
+  operator = input("Enter an operator (+ , - , * , / , ** ): ")
+  if operator == 'exit':
+            print("Exiting the calculator.")
+            break
+  num1 = float(input(f"Enter the 1st number: "))
+  num2 = float(input(f"Enter the 2nd number: "))
+
+   
+ 
+            
+
+  if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+  elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+  elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+  elif operator == "/":
+    if num2 == 0:
+     print(f"You can't do a division by number 0. Try again.")
+    if num2 == 0:
+     print(f"You can't do a division by number 0. Try again.")
+    else:
+        result = num1 / num2
+        print(round(result, 3))
+    result = num1 / num2
+    print(round(result, 3))
+  elif operator == "**":
+    result = num1 ** num2
+    print(round(result, 3))
+
+  else:
+    print(f"{operator} is not a valid operator")
+
+  if num2 == 0:
+   print(f"You can't do a division by number 0. Try again.")
+
+
+  if num1 == 0:
+    print(f"You can't do a division by number 0. Try again.")
