43adca73ecd5a3f4b8397accfd698ebbc780d87e
KandilDimos <142528384+KandilDimos@users.noreply.github.com>
2023-11-23 22:11:45 +0200

    Update Calculator with functions (2).py

--- Calculator with functions (2).py
+++ Calculator with functions (2).py
@@ -1,50 +1,44 @@
 def get_operator():
-   operator = input("Enter an operator (+ , - , / , * , **)")
-   if operator != "+" and  operator != "-" and operator != "/"  and operator != "*" and operator != "**":
-      print("Invalid operator. Try again")
-      get_operator()
-   return operator
-   
+    operator = input("Enter an operator (+ , - , /, * , **) or type 'exit' to exit. ")
+    if operator != "+" and  operator != "-" and operator != "/"  and operator != "*" and operator != "**" and operator != "exit":
+        print("Invalid operator. Try again.")
+        get_operator()
+    return operator
 
 def get_num1():
-   num1 = float(input("Enter the 1st number: "))
-   return num1
+    num1 = float(input("Enter the 1st number: "))
+    return num1
 
-def get_num2():   
-   num2 = float(input("Enter the 2nd number: "))
-   return num2
-   
-def operation(a,b,c):   
-   if a == "+":
-      result = b + c
-      return (round(result, 3))
-   elif a == "-":
-      result = b - c
-      return (round(result, 3))
-   elif a == "*":
-      result = b * c
-      return (round(result, 3))
-   elif a == "/":
-      if c == 0:
-         print(f"You can't do a division by number 0. Try again.")
-      else:
-         result = b / c
-         return (round(result, 3))
-   elif a == "**":
-      result = b ** c
-      return (round(result, 3))
+def get_num2():
+    num2 = float(input("Enter the 2nd number: "))
+    return num2
+
+def operation(a, b, c):
+    if a == "+":
+        return round(b + c, 3)
+    elif a == "-":
+        return round(b - c, 3)
+    elif a == "*":
+        return round(b * c, 3)
+    elif a == "/":
+        if c == 0:
+            print("You can't divide by 0. Try again.")
+        else:
+            return round(b / c, 3)
+    elif a == "**":
+        return round(b ** c, 3)
 
 def calc():
-   operator = get_operator()
-   while(operator != "exit"):
-      result = operation(operator,get_num1(),get_num2())
-      print(f"Result is = {result}")
-      operator = get_operator()
-   else:
-      print("You exited the programme.")  
+    while 1:
+        operator = get_operator()
+        if operator == "exit":
+            print("You just exited the programme.")
+            break
+        
+        num1 = get_num1()
+        num2 = get_num2()
+
+        result = operation(operator, num1, num2)
+        print(f"Result is = {result}")
 
 calc()
-
-
-
-
